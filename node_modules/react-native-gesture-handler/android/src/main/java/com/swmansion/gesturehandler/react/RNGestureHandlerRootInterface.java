b91abbccf9ce9cd4c965732de22b420cf7b284f6
mateuslopes92 <mateuslopes92@gmail.com>
2019-12-11 23:43:33 -0300

    Initialized project and reactotron configuration

---
+++ node_modules/react-native-gesture-handler/android/src/main/java/com/swmansion/gesturehandler/react/RNGestureHandlerRootInterface.java
@@ -0,0 +1,7 @@
+package com.swmansion.gesturehandler.react;
+
+import android.support.annotation.Nullable;
+
+public interface RNGestureHandlerRootInterface {
+  @Nullable RNGestureHandlerRootHelper getRootHelper();
+}
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import 'react-native-gesture-handler';
+import './config/ReactotronConfig';
+
+import Routes from './routes';
+
+// import { Container } from './styles';
+
+console.tron.log('teste');
+
+export default function App() {
+  return <Routes />;
+}
